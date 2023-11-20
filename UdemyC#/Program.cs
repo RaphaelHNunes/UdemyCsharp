de290a85a0e6d5fed61330bcf9f72ad34952be1e
RaphaelHNunes <raafael_henrique@hotmail.com.br>
2023-11-20 13:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ UdemyC#/Program.cs
@@ -0,0 +1,29 @@
+﻿// TIPOS DE DADOS/VALOR
+bool completo = false;// VARIAVEL QUE RECEBE SOMENTE DOIS TIPOS DE VALOR
+char genero = 'F';
+char letra = '\u0041'; //CODIGO ENCONTRADO NA TABELA UNICODE. 
+byte n1 = 126;
+int n2 = 1000;
+int n3 = 2147483647;
+long n4 = 2147483647L; //# 
+float n5 = 4.5f;
+double n6 = 4.5;
+string nome = "Maria Green";
+object obj1 = "Alex Brown";
+obj1 = n1;
+int n7 = int.MaxValue; //comando utilizado para saber o maximo que uma string pode receber. 
+
+
+
+Console.WriteLine(completo);
+Console.WriteLine(genero);
+Console.WriteLine(letra);
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+Console.WriteLine(n4);
+Console.WriteLine(n5);
+Console.WriteLine(n6);
+Console.WriteLine(nome);
+Console.WriteLine(obj1);
+Console.WriteLine(n7);
